3e4f57a0ea99b08ba5c50b4a05b854e284f8a426
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-07-02 14:14:56 +0700

    add spec to check cell with zero col index

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,7 +1,35 @@
+require './lib/cell'
 
 describe "cell" do
   it  "can calculate size of an array" do
     array_cell = Array.new
     expect(array_cell.empty?).to be true
   end
+
+  it "can check of each cell" do
+    test_cell = GameOfLife::Cell.new
+    test_cell.grid
+    (0...7).each do |row|
+      row = Array.new
+        (0...7).each do |cell|
+          row.push(0)
+        end
+        test_cell.grid.push(row)
+      end
+      expect(7).to eq(test_cell.minimal_column(8))
+  end
+
+  it "can check of each cell with zero params" do
+    test_cell = GameOfLife::Cell.new
+    test_cell.grid
+    (0...7).each do |row|
+      row = Array.new
+        (0...7).each do |cell|
+          row.push(0)
+        end
+        test_cell.grid.push(row)
+      end
+      expect(0).to eq(test_cell.minimal_column(0))
+  end
+
 end
